993f8d64d0522f90d5099e7b149b019f99bc88e6
ludooo2204 <vachon.ludovic@gmail.com>
2021-10-25 20:45:15 +0200

    www

--- src/www/API_menu/postPlat.php
+++ src/www/API_menu/postPlat.php
@@ -1,4 +1,4 @@
-²<?php
+<?php
 
 
 header('Access-Control-Allow-Origin: *');
@@ -16,15 +16,26 @@ die();
 
 	$json = file_get_contents(('php://input'));
     $object = json_decode($json);
- 
-    $nom_ingredient = $object->nom_ingredient;
+    $platNom = $object->platNom;
+    $platMidiSoir = join(',', $object->platMidiSoir);
+    $platType = join(',', $object->platType);
+    $platTypeViande = join(',', $object->platTypeViande);
+    $platUrl = $object->platUrl;
+    $platVitesse = $object->platVitesse;
+    $saison = join(',', $object->saison);
+    $ingredientsChoisi = join(',', $object->ingredientsChoisi);
+    var_dump($object);
+
 try {
     $pdo = new PDO("mysql:host=127.0.0.1;dbname=menu;charset=utf8", "root", "", [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]);
 
 //  $pdo = new PDO("mysql:host=127.0.0.1;dbname=drpnngev_api;charset=utf8", "drpnngev_ludo", "q!2R(O9EJss6i0", [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]);
 
 
-    $query= $pdo->prepare("INSERT INTO ingredients (nom_ingredient) VALUES('" . $nom_ingredient . "')");
+    $query= $pdo->prepare("INSERT INTO plats(nom_plat,saison,midiSoir) VALUES('" . $platNom . "', '" . $saison . "', '" . $platMidiSoir . "')");
+
+
+    // $query= $pdo->prepare("INSERT INTO aideMemoire(description, categorie, nom) VALUES('" . $description . "', '" . $categorie . "', '" . $nom . "')");
     $query->execute();
 
 
